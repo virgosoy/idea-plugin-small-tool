1ddeea41471bb5d6b11eac63cb7de8e6f4ae04b0
GitHub Action <action@github.com>
2021-08-22 12:42:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/virgosoy/ideapluginsmalltool/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.virgosoy.ideapluginsmalltool.services
+
+import com.github.virgosoy.ideapluginsmalltool.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/virgosoy/ideapluginsmalltool/services/MyProjectService.kt
+++ src/main/kotlin/com/github/virgosoy/ideapluginsmalltool/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.virgosoy.ideapluginsmalltool.services
 
+import com.github.virgosoy.ideapluginsmalltool.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
